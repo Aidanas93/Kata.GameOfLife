3b4d602d20909cc39961c4e4379a0f4c304b7fe2
Aidanas93 <aidanas93@gmail.com>
2020-08-17 17:51:17 +0300

    Finished kata

---
+++ Kata.GameOfLife/GameBoard.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Kata.GameOfLife
+{
+    public class GameBoard : ICloneable
+    {
+        public char[,] board;
+        public readonly int rows;
+        public readonly int columns;
+
+        public GameBoard(char[,] board, int rows, int columns)
+        {
+            this.board = board;
+            this.rows = rows;
+            this.columns = columns;
+        }
+
+        public object Clone()
+        {
+            return new GameBoard((char[,])board.Clone(), rows, columns);
+        }
+    }
+}
